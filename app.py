9f817f5582c6a6ff9f209b30f4fbf93b1eddd406
jvan <jordan.vanaalsburg@gmail.com>
2015-03-01 13:54:28 -0800

    Added sign-up api endpoint.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.ext.cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,17 @@ CORS(app, resources=r'/api/*', allow_headers='Content-Type')
 @app.route('/api/')
 def ping():
     return jsonify({"status": "ok"})
+
+@app.route('/api/sign-up', methods=['POST'])
+def sign_up():
+    data = request.json
+
+    first = data['first']
+    last  = data['last']
+    email = data['email']
+    ip_addr = request.remote_addr
+
+    return jsonify({'user': {'first': first, 'last': last, 'email': email, 'ip_addr': ip_addr}})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
