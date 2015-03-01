ebbacdb12d6ce729b474a73b9b6ff81b29cd4fab
jvan <jordan.vanaalsburg@gmail.com>
2015-03-01 14:37:12 -0800

    Store data in mongo db.

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ from flask.ext.cors import CORS
 app = Flask(__name__)
 CORS(app, resources=r'/api/*', allow_headers='Content-Type')
 
+import os
+
+host = os.environ['DB_PORT_27017_TCP_ADDR']
+port = os.environ['DB_PORT_27017_TCP_PORT']
+
+from pymongo import MongoClient
+
+client = MongoClient(host, int(port))
+db = client['davisgig']
+
 @app.route('/api/')
 def ping():
     return jsonify({"status": "ok"})
@@ -16,6 +26,12 @@ def sign_up():
     last  = data['last']
     email = data['email']
     ip_addr = request.remote_addr
+
+    db.contacts.insert({
+        'first': first,
+        'last': last,
+        'email': email,
+        'ip_addr': ip_addr})
 
     return jsonify({'user': {'first': first, 'last': last, 'email': email, 'ip_addr': ip_addr}})
 
